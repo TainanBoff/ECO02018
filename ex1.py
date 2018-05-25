668eaa4996cb291bd5f34cd6081950b2de6a4399
TainanBoff <tainanboff@gmail.com>
2018-05-25 18:52:23 -0300

    exerciico 1 da aula de 28/05

---
+++ ex1.py
@@ -0,0 +1,13 @@
+"""
+
+Nome: ex1
+Descricao: Atribui o valor 100.000 a variavel consumoagregado.
+Autores: Bruno e Tainan
+data: 25/05/2018
+Versao: 0.0.1
+
+"""
+
+# Entrada de dados
+
+consumoagregado = 100000
